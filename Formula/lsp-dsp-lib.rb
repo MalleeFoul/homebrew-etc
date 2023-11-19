6f9bf228a87994969b885c0a15cae198b103208f
MalleeFoul <visittheinfidelw.expln.pamphlets@gmail.com>
2023-11-19 16:14:34 +1100

    This is painful

--- Formula/lsp-dsp-lib.rb
+++ Formula/lsp-dsp-lib.rb
@@ -4,26 +4,22 @@
 class LspDspLib < Formula
   desc ""
   homepage ""
-  url "https://github.com/lsp-plugins/lsp-dsp-lib/archive/refs/tags/1.0.19.zip"
-  sha256 "11e4c8da6c4f6761a00e8dc80fa9b8f4c1493f240fc095cb3b9c446a2d075bac"
+  url "https://github.com/lsp-plugins/lsp-dsp-lib/releases/download/1.0.19/lsp-dsp-lib-src-1.0.19.tar.gz"
+  sha256 "541e3c50c10035985e98a9a4772dc76bf8588b7187747e1fa5cd2692d1ded28d"
   license ""
 
-  # depends_on "cmake" => :build
   depends_on "make" => :build
   depends_on "malleefoul/etc/lsp-common-lib" => :build
   fails_with :clang
 
+  patch :DATA
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "make", "config", "PREFIX=#{prefix}", "LIBDIR=#{lib}"
-    system "make", "fetch"
-    system "make"
-    system "make", "install"
-    
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "gmake", "config", "PREFIX=#{prefix}"
+    system "gmake"
+    system "gmake", "install"
   end
 
   test do
@@ -39,3 +35,28 @@ class LspDspLib < Formula
     system "false"
   end
 end
+__END__
+diff --git a/make/tools.mk b/make/tools.mk
+index 393707f..c9c3806 100644
+--- a/make/tools.mk
++++ b/make/tools.mk
+@@ -62,7 +62,7 @@ GIT                ?= $(X_GIT_TOOL)
+ INSTALL            ?= $(X_INSTALL_TOOL)
+ 
+ # Patch flags and tools for (cross) build
+-FLAG_RELRO          = -Wl,-z,relro,-z,now
++#~ FLAG_RELRO          = -Wl,-z,relro,-z,now
+ FLAG_STDLIB         = 
+ CFLAGS_EXT          = $(ARCHITECTURE_CFLAGS)
+ CXXFLAGS_EXT        = $(ARCHITECTURE_CFLAGS)
+@@ -162,8 +162,8 @@ CXXDEFS            += -DLSP_INSTALL_PREFIX=\\\"$(PREFIX)\\\"
+ 
+ INCLUDE            :=
+ LDFLAGS            := $(LDFLAGS_EXT) -r
+-EXE_FLAGS          := $(EXE_FLAGS_EXT) $(FLAG_RELRO) -Wl,--gc-sections
+-SO_FLAGS           := $(SO_FLAGS_EXT) $(FLAG_RELRO) -Wl,--gc-sections -shared $(FLAG_STDLIB) -fPIC 
++EXE_FLAGS          := $(EXE_FLAGS_EXT) $(FLAG_RELRO) -Wl,-dead_strip
++SO_FLAGS           := $(SO_FLAGS_EXT) $(FLAG_RELRO) -Wl,-dead_strip -shared $(FLAG_STDLIB) -fPIC 
+ 
+ # Define flags for host build
+ HOST_CFLAGS        := $(CFLAGS)
